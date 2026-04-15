0852945516f0ea88d8cfaadcb3e187420ce17fab
aaravjais600-oss <aaravjais600@gmail.com>
2026-04-15 20:24:20 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+from config import MONGO_URL
+
+client = MongoClient(MONGO_URL)
+db = client["telegram_bot"]
+
+# ✅ collections
+users = db["users"]
+settings = db["settings"]
+
+
+# =========================
+# USER FUNCTIONS
+# =========================
+def add_user(user_id):
+    if not users.find_one({"user_id": user_id}):
+        users.insert_one({
+            "user_id": user_id
+        })
+
+
+def get_all_users():
+    return list(users.find())
+
+
+# =========================
+# SETTINGS FUNCTIONS
+# =========================
+def set_setting(key, value):
+    settings.update_one(
+        {"key": key},
+        {"$set": {"value": value}},
+        upsert=True
+    )
+
+
+def get_setting(key, default=None):
+    data = settings.find_one({"key": key})
+    return data["value"] if data else default
