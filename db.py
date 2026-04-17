64ba9345b12307d543491ec565ea0e478d8b590d
aaravjais600-oss <aaravjais600@gmail.com>
2026-04-17 16:57:42 +0530

    Update db.py

--- db.py
+++ db.py
@@ -13,6 +13,7 @@ settings = db["settings"]
 # USER FUNCTIONS
 # =========================
 def add_user(user_id):
+    user_id = int(user_id)
     if not users.find_one({"user_id": user_id}):
         users.insert_one({
             "user_id": user_id
